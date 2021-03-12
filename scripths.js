1169fb2f5f34e16441eaf75c6be1af91faea35ab
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-03-12 14:25:13 -0600

    Macbook

--- scripths.js
+++ scripths.js
@@ -1,19 +1,32 @@
 var submitBtn = document.querySelector(".saveBtn");
 var inputText = document.querySelector(".inputSave")
 var localDisplay = document.getElementById("display-scores");
-var scoreMsg = document.getElementById("score-message");
+var scoreMsg = document.querySelector("#score-message");
+window.addEventListener("load", displayScore);
+var scoreDisplay = localStorage.getItem("score");
+var cleanBtn = document.querySelector(".clean-local");
 submitBtn.addEventListener("click", saveLocal);
+
+var scoreMessage = document.createElement("p");
+var localEL = document.createElement("p");
+
 function saveLocal(){
     localStorage.setItem("GameName", inputText.value);
     var displayLocal = localStorage.getItem("GameName");
-    var localEL = document.createElement("p");
-    localEL.textContent = displayLocal;
+    localEL.textContent = displayLocal + " " + scoreDisplay;
     localDisplay.appendChild(localEL);
-    console.log(displayLocal);
-    var scoreDisplay = localStorage.getItem("score");
-    var scoreMessage = document.createElement("p");
-    scoreMessage.textContent = "Your score was" + scoreDisplay;
-    scoreMsg.appendChild(scoreMessage);
-    inputText.value = ""; 
 }
 
+function displayScore (){
+scoreMessage.textContent = "Your score was " + scoreDisplay;
+scoreMsg.appendChild(scoreMessage);
+}
+
+cleanBtn.addEventListener("click", cleanQuery);
+
+function cleanQuery(){
+    localStorage.removeItem("score");
+    localStorage.removeItem("GameName");
+    scoreMessage.textContent = "";
+    localEL.textContent = "";
+}
